3934fe2a7477e845df0e15287aa3af990852b337
marce oselin <marce.oselin@gmail.com>
2023-07-25 14:46:31 -0300

    Ejercicio A y B terminado

---
+++ trabajo-a.js
@@ -0,0 +1,41 @@
+console.log("Ejercicio A : ")
+console.log(" ")
+
+var cantidadDeGatos = 10;
+
+
+for (var i = 1; i <= cantidadDeGatos; i++) {
+
+  if (i % 3 === 1) {
+    console.log("Gato #" + i + ": 😺");
+  } else if (i % 3 === 2) {
+    console.log("Gato #" + i + ": 😸");
+  } else {
+    console.log("Gato #" + i + ": 😹");
+  }
+}
+console.log(" ")
+
+var cantidadDeGatos= 8;
+for (var i = 1; i <= cantidadDeGatos; i++) {
+    if (i % 3 === 1) {
+        console.log("Gato #" + i + ": 😺");
+      } else if (i % 3 === 2) {
+        console.log("Gato #" + i + ": 😸");
+      } else {
+        console.log("Gato #" + i + ": 😹");
+      }
+    }
+    console.log(" ")
+
+
+    var cantidadDeGatos= 4;
+    for (var i = 1; i <= cantidadDeGatos; i++) {
+        if (i % 3 === 1) {
+            console.log("Gato #" + i + ": 😺");
+          } else if (i % 3 === 2) {
+            console.log("Gato #" + i + ": 😸");
+          } else {
+            console.log("Gato #" + i + ": 😹");
+          }
+        }    
---
+++ trabajo-b.js
@@ -0,0 +1,27 @@
+
+
+
+
+var cantidadDeGatos = 12
+var cantidadDePasos = 4
+
+for(i=1; i<= cantidadDeGatos; i++){
+    console.log("gato # " + i + " : 🐈 🐾 🐾 🐾 🐾")
+}
+console.log(" ")
+
+var cantidadDeGatos = 6;
+var cantidadDePasos = 3;
+
+for(i=1; i<= cantidadDeGatos; i++){
+    console.log("gato # " + i + " : 🐈 🐾 🐾 🐾 ")
+}
+console.log(" ")
+
+var cantidadDeGatos = 5;
+var cantidadDePasos = 1;
+
+for(i=1; i<= cantidadDeGatos; i++){
+    console.log("gato # " + i + " : 🐈 🐾  ")
+}
+
