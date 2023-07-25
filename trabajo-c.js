603e85104e8f1617e7678f6eb2897eb890f5bd2d
marce oselin <marce.oselin@gmail.com>
2023-07-25 15:06:43 -0300

    Agrego archivo c

---
+++ trabajo-c.js
@@ -0,0 +1,12 @@
+ 
+
+ var cantidadDeGatos = 15;
+ var cantidadDePasos = 3;
+
+ for( var i = 1; i<= cantidadDeGatos; i++){
+    if( i% 2 == 0){
+        console.log("gato #  " + i + ": 🐈 🐾 🐾 🐾")
+    }else{
+        console.log("gato # " + i + "🐈⬛ 🐾 🐾 🐾")
+    }
+ }
